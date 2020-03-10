44ebe4586747537d58863461eba8eb7e048d843a
Flamingx64 <61998256+Flamingx64@users.noreply.github.com>
2020-03-10 19:33:05 +0700

    Add files via upload

---
+++ mwehehe.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char yt;
+
+
+awal:
+cout<<"mau gak jadi pacarku!"<<endl<<endl;
+cout<<" [Y/T] : ";
+cin>>yt;
+cout<<endl;
+
+
+if(yt=='Y' || yt=='y')
+cout<<"yeay akhirnya punya pacar";
+else
+cout<<"yahh ditolak hiks";
+}
